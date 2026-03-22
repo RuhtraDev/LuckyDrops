a2dd26eaff838cc3be7eefdc7c86e1ded101da10
RuhtraDev <ruhtra.dev@gmail.com>
2026-03-22 12:48:51 +0000

    Update corrections

--- src/index.ts
+++ src/index.ts
@@ -274,15 +274,15 @@ function fixOcrText(text: string): string {
     const corrections = [
         { wrong: "haif", correct: "half" },
         { wrong: "tooth haif", correct: "tooth half" },
-        { wrong: "Loof", correct: "Loop" },
-    /*  { wrong: "haly", correct: "half" },
+        { wrong: "loof", correct: "loop" },
+        { wrong: "haly", correct: "half" },
         { wrong: "hailf", correct: "half" },
         { wrong: "haff", correct: "half" },
         { wrong: "tooth haif of a key", correct: "tooth half of a key" },
         { wrong: "loof haif", correct: "loop half" },
         { wrong: "loof half", correct: "loop half" },
         { wrong: "tooth half", correct: "tooth half" },
-        { wrong: "loop half", correct: "loop half" }*/
+        { wrong: "loop half", correct: "loop half" }
     ];
     
     for (let i = 0; i < corrections.length; i++) {
@@ -717,7 +717,7 @@ function showItems() {
         return "📦";
     }
     
-
+    
     if (mode === "total") {
         listHeader.innerHTML = "Drop Totals";
         listHeader.dataset.show = "history";
@@ -741,7 +741,7 @@ function showItems() {
             let iconDisplay = "";
             let itemStyle = "";
             
-            if ((currentFilter === "COMPS" || currentFilter === "all") && isRareComponent) {
+           if ((currentFilter === "COMPS" || currentFilter === "all") && isRareComponent) {
                 iconDisplay = "🔴";
                 itemStyle = ' style="color: #ff0000 !important; font-weight: bold !important;"';
             } else if (currentFilter === "COMPS" && !isRareComponent) {
